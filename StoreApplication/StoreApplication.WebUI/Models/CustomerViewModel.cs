26b6746213610ff10cbb89101c7cc64a0acedbd6
Rodrigo Salomon <rodsalomon10@hotmail.com>
2019-05-15 11:27:24 -0500

    new commits

--- StoreApplication/StoreApplication.WebUI/Models/CustomerViewModel.cs
+++ StoreApplication/StoreApplication.WebUI/Models/CustomerViewModel.cs
@@ -13,9 +13,11 @@ namespace StoreApplication.WebUI.Models
         [Display(Name = "ID")]
         public int Id { get; set; }
 
+        [Display(Name = "First Name")]
         [Required]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         [Required]
         public string LastName { get; set; }
 
